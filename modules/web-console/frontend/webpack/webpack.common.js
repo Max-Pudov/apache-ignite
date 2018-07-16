f8a3cdc3c0daf67fe62fe2a483c06f8b63ce5d73
alexdel <verbalab@yandex.ru>
2018-07-16 17:22:42 +0700

    WC-567. Web Console: Make icons bundlingg correctly.

--- modules/web-console/frontend/webpack/webpack.common.js
+++ modules/web-console/frontend/webpack/webpack.common.js
@@ -75,9 +75,11 @@ const config = {
             // Exclude tpl.pug files to import in bundle.
             {
                 test: /^(?:(?!tpl\.pug$).)*\.pug$/, // TODO: check this regexp for correct.
-                loader: 'pug-html',
-                query: {
-                    basedir
+                use: {
+                    loader: 'pug-html',
+                    options: {
+                        basedir
+                    }
                 }
             },
 
@@ -124,21 +126,21 @@ const config = {
             {
                 test: /\.(ttf|eot|svg|woff(2)?)(\?v=[\d.]+)?(\?[a-z0-9#-]+)?$/,
                 exclude: [contentBase],
-                loader: 'file?name=assets/fonts/[name].[ext]'
+                use: 'file?name=assets/fonts/[name].[ext]'
             },
             {
                 test: /^(?:(?!url\.svg$).)*\.svg$/,
                 include: [contentBase],
-                use: ['svg-sprite-loader']
+                use: 'svg-sprite-loader'
             },
             {
                 test: /.*\.url\.svg$/,
                 include: [contentBase],
-                loader: 'file?name=assets/fonts/[name].[ext]'
+                use: 'file?name=assets/fonts/[name].[ext]'
             },
             {
                 test: /\.(jpe?g|png|gif)$/i,
-                loader: 'file?name=assets/images/[name].[hash].[ext]'
+                use: 'file?name=assets/images/[name].[hash].[ext]'
             },
             {
                 test: require.resolve('jquery'),
@@ -149,7 +151,7 @@ const config = {
             },
             {
                 test: require.resolve('nvd3'),
-                use: ['expose-loader?nv']
+                use: 'expose-loader?nv'
             }
         ]
     },
